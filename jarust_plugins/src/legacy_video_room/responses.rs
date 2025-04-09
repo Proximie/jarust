326954e05ea3017f9d6c422bd0b643a599772d4d
Ghamza-Jd <hamzah.jadeed@gmail.com>
2025-04-10 00:40:42 +0300

    feat(legacy-video-room): added event parsing

--- jarust_plugins/src/legacy_video_room/responses.rs
+++ jarust_plugins/src/legacy_video_room/responses.rs
@@ -12,3 +12,12 @@ pub struct LegacyVideoRoomExistsRsp {
     pub room: JanusId,
     pub exists: bool,
 }
+
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug, Deserialize)]
+pub struct LegacyVideoRoomPublisher {
+    /// unique ID of active publisher
+    pub id: JanusId,
+    /// display name of active publisher
+    pub display: Option<String>,
+    pub substream: Option<u8>,
+}
