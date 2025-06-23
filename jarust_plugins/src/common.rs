b9a5c9f886269c303bbe81d26a99a0a6e5e2dc46
Matthieu Rogez <matthieu.rogez@fivesgroup.com>
2025-06-23 14:39:47 +0000

    refactor: rework U63 instantiation and serialization
    * remove new_saturated / new_wrapping
    * gather ffi dependant functions in U63 impl (new, inner)
    These functions are private on purpose, use conversion From / TryFrom as
    needed.
    * TryFrom now returns a TryFromIntError
    * Handle problematic case of deserialization of a value larger than
      U63::MAX
    * Add serialization / deserialization tests

--- jarust_plugins/src/common.rs
+++ jarust_plugins/src/common.rs
@@ -1,3 +1,4 @@
+use serde::de;
 use serde::Deserialize;
 use serde::Serialize;
 
@@ -32,73 +33,45 @@ pub struct U63(u64);
 impl U63 {
     pub const MAX: u64 = (1 << 63) - 1;
 
-    pub fn new(value: u64) -> Self {
-        Self::new_wrapping(value)
-    }
-
     #[cfg(feature = "ffi-compatible")]
-    pub fn new_wrapping(value: u64) -> Self {
-        Self {
-            inner: value & U63::MAX,
-        }
-    }
-
-    #[cfg(feature = "ffi-compatible")]
-    pub fn new_saturating(value: u64) -> Self {
-        if value > U63::MAX {
-            Self { inner: U63::MAX }
-        } else {
-            Self { inner: value }
-        }
+    fn new(value: u64) -> Self {
+        Self { inner: value }
     }
 
     #[cfg(not(feature = "ffi-compatible"))]
-    pub fn new_wrapping(value: u64) -> Self {
-        Self(value & U63::MAX)
+    fn new(value: u64) -> Self {
+        Self(value)
+    }
+
+    #[cfg(feature = "ffi-compatible")]
+    fn inner(&self) -> u64 {
+        self.inner
     }
 
     #[cfg(not(feature = "ffi-compatible"))]
-    pub fn new_saturating(value: u64) -> Self {
-        if value > U63::MAX {
-            Self(U63::MAX)
-        } else {
-            Self(value)
-        }
+    fn inner(&self) -> u64 {
+        self.0
     }
 }
 
 impl TryFrom<u64> for U63 {
-    type Error = &'static str;
+    type Error = std::num::TryFromIntError;
 
-    #[cfg(feature = "ffi-compatible")]
     fn try_from(value: u64) -> Result<Self, Self::Error> {
         if value > U63::MAX {
-            Err("U63 only accepts values lower than 2^63 - 1")
+            // Work around impossible instanciation from outside of std
+            // by generating a known to fail conversion.
+            // Should compile to nothing with optimisation level other than 0.
+            Err(<u8 as TryFrom<u16>>::try_from(300).unwrap_err())
         } else {
-            Ok(Self { inner : value })
+            Ok(U63::new(value))
         }
     }
-
-    #[cfg(not(feature = "ffi-compatible"))]
-    fn try_from(value: u64) -> Result<Self, Self::Error> {
-        if value > U63::MAX {
-            Err("U63 only accepts values lower than 2^63 - 1")
-        } else {
-            Ok(Self(value))
-        }
-    }
-
 }
 
 impl From<U63> for u64 {
-    #[cfg(feature = "ffi-compatible")]
     fn from(value: U63) -> Self {
-        value.inner
-    }
-
-    #[cfg(not(feature = "ffi-compatible"))]
-    fn from(value: U63) -> Self {
-        value.0
+        value.inner()
     }
 }
 
@@ -117,18 +90,34 @@ impl<'de> Deserialize<'de> for U63 {
         D: serde::Deserializer<'de>,
     {
         let value = u64::deserialize(deserializer)?;
-        Ok(U63::new(value))
+        U63::try_from(value).map_err(|_| -> D::Error {
+            de::Error::invalid_value(
+                de::Unexpected::Unsigned(value.into()),
+                &"expecting an u64 lower or equal to 9223372036854775807",
+            )
+        })
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::common::U63;
+    use super::U63;
+    use serde_test::{assert_de_tokens_error, assert_tokens, Token};
 
     #[test]
     fn test_u63_conversion_with_u64() {
         assert_eq!(u64::from(U63::try_from(123_456u64).unwrap()), 123_456u64);
         assert_eq!(u64::from(U63::try_from(U63::MAX).unwrap()), U63::MAX);
         assert!(U63::try_from(U63::MAX + 1).is_err());
+    }
+
+    #[test]
+    fn test_u63_serialization() {
+        assert_tokens(
+            &U63::try_from(123_456u64).unwrap(),
+            &[Token::U64(123_456u64)],
+        );
+        assert_tokens(&U63::try_from(U63::MAX).unwrap(), &[Token::U64(U63::MAX)]);
+        assert_de_tokens_error::<U63>(&[Token::U64(U63::MAX + 1)], "invalid value: integer `9223372036854775808`, expected expecting an u64 lower or equal to 9223372036854775807");
     }
 }
